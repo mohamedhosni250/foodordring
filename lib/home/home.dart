5da60698a3a20104703e0eea2709f39648cf8e9a
mohamedhosni <mohamed.hosni3202@gmail.com>
2022-09-30 17:57:22 +0200

    first commit

---
+++ lib/home/home.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:foodapp/utils/colors.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+            margin: EdgeInsets.only(top: 25, bottom: 15),
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(children: const [
+                  Text('Country'),
+                  Text('Qena'),
+                ]),
+                Container(
+                  width: 45,
+                  height: 45,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: AppColors.mainColor),
+                  child: const Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            )),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/primary_text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PrimaryText extends StatelessWidget {
+  const PrimaryText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
